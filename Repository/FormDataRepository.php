0f167c5e7d870f4fa0f037b259da83f97fc74ec3
Oliver Kossin <thecadiens@gmail.com>
2021-10-17 08:59:36 +0200

    init repo

---
+++ Repository/FormDataRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Alengo\Bundle\AlengoFormBundle\Repository;
+
+
+use Alengo\Bundle\AlengoFormBundle\Entity\FormData;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method FormData|null find($id, $lockMode = null, $lockVersion = null)
+ * @method FormData|null findOneBy(array $criteria, array $orderBy = null)
+ * @method FormData[]    findAll()
+ * @method FormData[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FormDataRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, FormData::class);
+    }
+
+    /**
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function save(FormData $formData): void
+    {
+        $this->getEntityManager()->persist($formData);
+        $this->getEntityManager()->flush();
+    }
+}
