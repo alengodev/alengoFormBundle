465fab07ca07d1c851620f7cdc9722fdb7793cb7
Oliver Kossin <thecadiens@gmail.com>
2021-10-18 08:16:19 +0200

    init test suit

---
+++ Tests/Application/Kernel.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of TheCadien/SuluObjectContentBundle
+ *
+ * (c) Oliver Kossin
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Alengo\Bundle\AlengoFormBundle\Tests\Application;
+
+use Alengo\Bundle\AlengoFormBundle\AlengoFormBundle;
+use Sulu\Bundle\TestBundle\Kernel\SuluTestKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+class Kernel extends SuluTestKernel
+{
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): array
+    {
+        /** @var BundleInterface[] $bundles */
+        $bundles = parent::registerBundles();
+        $bundles[] = new AlengoFormBundle();
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        parent::registerContainerConfiguration($loader);
+        $loader->load(__DIR__.'/config/config.yml');
+    }
+
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+}
---
+++ Tests/Unit/Entity/Factory/FormDataFactoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alengo\Bundle\AlengoFormBundle\Tests\Unit\Entity\Factory;
+
+use PHPUnit\Framework\TestCase;
+
+class FormDataFactoryTest extends TestCase
+{
+    public function testFactory()
+    {
+        self::assertSame(true,true);
+    }
+}
