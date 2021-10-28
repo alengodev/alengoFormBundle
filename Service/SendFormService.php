733f8523af19c246c857fdf19e0dcdc3fe29e6c2
Oliver Kossin <thecadiens@gmail.com>
2021-10-28 19:02:54 +0200

    Set Mailer Service

--- Service/SendFormService.php
+++ Service/SendFormService.php
@@ -3,12 +3,40 @@
 namespace Alengo\Bundle\AlengoFormBundle\Service;
 
 use Alengo\Bundle\AlengoFormBundle\Entity\FormData;
+use Swift_Mailer;
+use Twig\Environment;
+
 
 class SendFormService implements SendFormInterface
 {
 
-    public function sendFormDataAsMail(FormData $formData, string $template, string $senderMail, string $receiverMail,bool $copy = false)
+    private Environment $twig;
+
+    public function __construct(Swift_Mailer $mailer, Environment $twig)
     {
-        // TODO: Implement sendFormDataAsMail() method.
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+    }
+
+    public function sendFormDataAsMail(FormData $formData, string $template, string $title)
+    {
+        $message = (new \Swift_Message($title))
+            ->setFrom($formData->getReceiverMail())
+            ->setBody(
+                $this->twig->render(
+                    $template,
+                    $formData->getData()
+                ),
+                'text/html'
+            );
+
+        if (!$formData->isCopy()) {
+            $message->setTo($formData->getReceiverMail());
+        } else {
+            $message->setTo([$formData->getReceiverMail(), $formData->getUserMail()]);
+        }
+
+
+        $this->mailer->send($message);
     }
 }
