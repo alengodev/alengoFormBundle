a44f11a75bfad9ae2641a8b3eea043edb2bb3614
Oliver Kossin <thecadiens@gmail.com>
2022-10-05 17:34:18 +0200

    Allow arrays in Form Data

--- Tests/Unit/Traits/FormDataTrait.php
+++ Tests/Unit/Traits/FormDataTrait.php
@@ -20,4 +20,18 @@ trait FormDataTrait
 
         return $data;
     }
+
+    public function generateFormDataWithArray(): FormData
+    {
+        $data = new FormData();
+        $data->setWebspaceKey('alengo');
+        $data->setData(['firstname' => 'Oliver','arrayData' => ['1' => 'test']]);
+        $data->setUserMail('usertest@test.de');
+        $data->setReceiverMail('receivertest@test.de');
+        $data->setLocale('de');
+        $data->setCreated(new \DateTime());
+        $data->setChanged(new \DateTime());
+
+        return $data;
+    }
 }
