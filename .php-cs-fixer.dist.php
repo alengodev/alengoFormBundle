7faaec2ce501a211b450df51e54dae6d71e78b6e
Alexander Huber <alex.huber@alengo.at>
2023-11-30 19:50:03 +0100

    (c) alengo

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Alengo\Bundle\AlengoFormBundle.
  *
- * (c) Alengo
+ * (c) alengo
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
@@ -14,7 +14,7 @@ declare(strict_types=1);
 $header = <<<'EOF'
 This file is part of Alengo\Bundle\AlengoFormBundle.
 
-(c) Alengo
+(c) alengo
 
 This source file is subject to the MIT license that is bundled
 with this source code in the file LICENSE.
